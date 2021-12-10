7b04dd1b37757753caf4ddd2098f2594d4329c5d
Parth Patel <ppatil91099@gmail.com>
2021-12-10 16:21:34 -0500

    change test

---
+++ stock-market/Twillio/Twillio.py
@@ -0,0 +1,18 @@
+from twilio.rest import Client
+from decouple import config
+
+
+FROM_NUMBER = config('T_TWILIO_NUMBER')
+ACCOUNT_SID = config('T_ACCOUNT_SID')
+AUTH_TOKEN = config('T_AUTH_TOKEN')
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+
+def send_sms(phone_number, message):
+    message = client.messages.create(
+        from_=FROM_NUMBER,
+        body=message,
+        to=phone_number
+    )
+    print(message.sid)
--- stock-market/test.py
+++ stock-market/test.py
@@ -1,9 +1,5 @@
-import sys
-sys.path.insert(0, 'E:/WorkSpace/stock-notify/twillio')
+from Twillio.Twillio import send_sms
 
-from send import send_sms
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    TO = '+19085241817'
-    BODY = 'Hello, this is a test message.'
-    send_sms(TO, BODY)
+    send_sms("+19085241817", "Hello World!")
