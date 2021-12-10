bcba83e35576bd01126f0580f0898627d7cf1276
Parth Patel <ppatil91099@gmail.com>
2021-12-10 16:49:14 -0500

    ?

--- stock-market/Twillio/Twillio.py
+++ stock-market/Twillio/Twillio.py
@@ -6,12 +6,12 @@ from decouple import config
 # ACCOUNT_SID = config('T_ACCOUNT_SID')
 # AUTH_TOKEN = config('T_AUTH_TOKEN')
 
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+client = Client(T_ACCOUNT_SID, T_AUTH_TOKEN)
 
 
 def send_sms(phone_number, message):
     message = client.messages.create(
-        from_=FROM_NUMBER,
+        from_=T_TWILIO_NUMBER,
         body=message,
         to=phone_number
     )
